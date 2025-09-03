7e6fc620209734a32e92396042a7d6f654b6322b
LEANDRO GARCIA <leogarcia1997@gmail.com>
2025-09-03 09:35:52 -0300

    Feat: Criação de Evento

---
+++ app/Filament/Resources/EventResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EventResource\Pages;
+use App\Filament\Resources\EventResource\RelationManagers;
+use App\Models\Event;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Toggle;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
+
+class EventResource extends Resource
+{
+    protected static ?string $model = Event::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label('Nome do Evento')
+                    ->placeholder('Jogos Amapaenses...')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('description')
+                    ->label('Descrição')
+                    ->placeholder('Evento esportivo...')
+                    ->maxLength(65535),
+                DatePicker::make('start_time')
+                    ->label('Início do Evento')
+                    ->required(),
+                DatePicker::make('end_time')
+                    ->label('Término do Evento'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->label('Nome do Evento')->sortable()->searchable(),
+                TextColumn::make('description')->label('Descrição')->limit(50)->sortable()->searchable()->default('--'),
+                TextColumn::make('start_time')->date()->sortable()->label('Início do Evento'),
+                TextColumn::make('end_time')->date()->sortable()->label('Término do Evento'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEvents::route('/'),
+            'create' => Pages\CreateEvent::route('/create'),
+            'edit' => Pages\EditEvent::route('/{record}/edit'),
+        ];
+    }
+}
